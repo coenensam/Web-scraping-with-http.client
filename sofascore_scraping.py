061c3d695dfd777e3b4df2d334362e2cadd86dc3
coenensam <162440198+coenensam@users.noreply.github.com>
2024-06-18 11:52:34 +0200

    Update sofascore_scraping.py

--- sofascore_scraping.py
+++ sofascore_scraping.py
@@ -4,29 +4,14 @@ import pandas as pd
 import numpy as np
 import time
 
-#######################################
+########################################
 
 # Set up connection
 conn = http.client.HTTPSConnection('www.sofascore.com')
 
 # Headers and cookies
-headers = {
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'cookie': '_gcl_au=1.1.1288433488.1716398667; _ga=GA1.1.1194105875.1716398669; FCCDCF=%5Bnull%2Cnull%2Cnull%2C%5B%22CP_BBcAP_BBcAEsACBENA1EoAP_gAEPgAA6II3gB5C5ETSFBYH51KIsUYAEHwAAAIsAgAAYBAQABQBKQAIQCAGAAEAhAhCACgAAAIEYBIAEACAAQAAAAAAAAIAAEIAAQAAAIICAAAAAAAABIAAAIAAAAEAAAwCAABAAA0AgEAJIISMgAAAAAAAAAAgAAAAAAAgAAAEhAAAEIAAAAACgAEABAEAAAAAEIABBII3gB5C5ETSFBYHhVIIMUIAERQAAAIsAgAAQBAQAAQBKQAIQCEGAAAAgAACAAAAAAIEQBIAEAAAgAAAAAAAAAIAAEAAAAAAAIICAAAAAAAABAAAAIAAAAAAAAwCAABAAAwQhEAJIASEgAAAAgAAAAAoAAAAAAAgAAAEhAAAEAAAAAAAAAEAAAEAAAAAAAABBIAAA.dnAACAgAAAA%22%2C%222~41.70.89.108.149.211.313.358.415.486.540.621.981.1029.1046.1092.1097.1126.1205.1301.1516.1558.1584.1598.1651.1697.1716.1753.1810.1832.1985.2328.2373.2440.2571.2572.2575.2577.2628.2642.2677.2767.2860.2878.2887.2922.3182.3190.3234.3290.3292.3331.10631~dv.%22%2C%22B315CD21-42CF-479E-9690-76340766A52E%22%5D%5D; _ga_3KF4XTPHC4=deleted; __gads=ID=066580bb9e47a032:T=1716398678:RT=1717964552:S=ALNI_MYMJ7zi9Rv1Ppj0kcXepXtIoIdNqg; __gpi=UID=00000e28926f6eee:T=1716398678:RT=1717964552:S=ALNI_MbbiKzlZdFtlQkAVeRmWduXuIuJQg; __eoi=ID=0911a3322fb7cce9:T=1716398678:RT=1717964552:S=AA-Afjbk-Y9tiW1fR53HpeI37td8; FCNEC=%5B%5B%22AKsRol-K0NTjXgkGNlCRddD58vfhKWqNINRiwBlDTqD5fcXh2wrEcn53pRvZuoTAiuENsh2J9IA3dr99lLYKl8VQ6oQ_S-GXHiPvh3Ss1Jf7yJEWyiXMK61zHmbF6RZtjkT0jCUERdGmYkXvpriRi8TcwsxyDXvs0w%3D%3D%22%5D%5D; _ga_QH2YGS7BB4=GS1.1.1717964327.43.1.1717964711.0.0.0; _ga_3KF4XTPHC4=GS1.1.1717964328.43.1.1717964711.60.0.0; _ga_HNQ9P9MGZR=GS1.1.1717964328.42.0.1717964711.60.0.0',
-    'if-none-match': 'W/"4bad0119b3"',
-    'referer': 'https://www.sofascore.com/tennis/2024-06-03',
-    'sec-ch-ua': '"Opera GX";v="109", "Not:A-Brand";v="8", "Chromium";v="123"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0 (Edition std-1)',
-    'x-requested-with': 'f92bd3',
-}
-headers['if-none-match']=''
+"""Use appropriate header to access the website"""
+headers = {}
 
 ########################################
 
@@ -59,7 +44,7 @@ df = df.set_index('match_id')
 
 conn.close()
 
-####################################
+########################################
 
 """ Find all matches in the span between two days, store the match id,
 the winner, the start time and the players."""
@@ -96,7 +81,7 @@ df = df.drop_duplicates()
 
 conn.close()
 
-#####################################
+########################################
 
 """Get odds for all matches in df"""
 
@@ -140,7 +125,7 @@ conn.close()
 df['odd home'] = odd_home
 df['odd away'] = odd_away
     
-####################################
+########################################
 
 """Get votes for all matches in df"""
 
@@ -164,7 +149,7 @@ conn.close()
 df['votes home'] = votes1
 df['votes away'] = votes2
 
-######################################
+########################################
 
 """Data analysis"""
 
